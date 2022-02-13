03caeea661333e9bd21ecf2b80a664c9f057c7cb
fh32000 <fh32000@gmail.com>
2022-02-13 23:23:23 +0300

    add Export Model Migration and Blade

---
+++ database/migrations/2022_02_13_183153_create_exports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exports', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('status')->default('in presses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exports');
+    }
+}
