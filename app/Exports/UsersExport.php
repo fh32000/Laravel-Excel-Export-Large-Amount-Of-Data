66f9ec0946cf1059b4d1d293369f20b5f0b68864
fh32000 <fh32000@gmail.com>
2022-02-13 23:25:15 +0300

    update export status in failed

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -8,12 +8,27 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
 
-class UsersExport implements FromQuery,ShouldQueue
+class UsersExport implements FromQuery, ShouldQueue
 {
     use Exportable, Queueable;
+
+    public $export;
+
+    public function __construct($export)
+    {
+        $this->export = $export;
+    }
 
     public function query()
     {
         return User::query();
     }
+
+    public function failed(Throwable $exception)
+    {
+        $this->export->update([
+            'status' => 'failed',
+        ]);
+    }
+
 }
